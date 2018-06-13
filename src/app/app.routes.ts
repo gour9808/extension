a612626426ae9dfe3f725371b7373a3d526f630c
gour9808 <gouravsharma614@gmail.com>
2018-06-13 12:44:35 +0530

    added modules (lazy loading)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import {ExtraOptions, RouterModule} from '@angular/router';
-import {NgModule} from '@angular/core';
+import { ExtraOptions, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
 import { ContainerComponent } from './container/container.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -8,24 +8,23 @@ import { DataLoaderComponent } from './data-loader/data-loader.component';
 import { EventsComponent } from './events/events.component';
 import { DiscussionsComponent } from './discussions/discussions.component';
 
-export const eventRoutes : Routes =[
-    
-    {path: 'all', component: EventsComponent},
+export const eventRoutes: Routes = [
+    { path: 'all', component: EventsComponent },
 
 ]
 
 export const dashboardRoutes: Routes = [
     { path: '', redirectTo: 'my', pathMatch: 'full' },
-    {path: 'my', component: DashboardComponent},
-    {path : 'events', loadChildren : 'app/events/events.module#EventsModule'}
+    { path: 'my', component: DashboardComponent },
+    { path: 'events', loadChildren: 'app/events/events.module#EventsModule' }
 ];
 
 export const appRoutes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     {
         path: 'home', component: ContainerComponent, loadChildren: 'app/dashboard/dashboard.module#DashboardModule'
-    
-    },
+
+    }
 
 ];
 
